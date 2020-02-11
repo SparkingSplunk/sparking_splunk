8fe4cbab02021a82f7898935fac7d6512df4a975
Shannen Jakosalem <shannen@jakosalem.net>
2020-02-11 14:25:15 +1100

    fixed data generator. to make everything work make sure to read the readme carefully

--- dataGen/curl.sh
+++ dataGen/curl.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# keep sending data until interrupt
 while true;
 do
 	curl -v -k "https://localhost:8088/services/collector" \
 		 -H "Authorization: Splunk d70e9818-7474-47f6-90cd-84cd9d38704a" \
-		 -d '{"event": "Hello, world!", "sourcetype": "manual"}'
+		 -d '{"event": "Hello, world!", "sourcetype": "manual", "source": "testsrc"}'
 	# "Hello, world!" sent every second
 	sleep 1
 done
