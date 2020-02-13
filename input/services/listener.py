d3cbe2811b42549e4878bce99f5a4dc8e6873a2d
David Young <53588304+dy27@users.noreply.github.com>
2020-02-13 20:21:03 +1100

    hello

--- input/services/listener.py
+++ input/services/listener.py
@@ -15,38 +15,34 @@ OUTPUT_PORT = 9001
 while True:
     print("Trying to reconnect ...")
     try:
-        input_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        output_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as input_socket:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as output_socket:
 
-        input_socket.bind((INPUT_HOST, INPUT_PORT))
-        output_socket.bind((OUTPUT_HOST, OUTPUT_PORT))
+                input_socket.bind((INPUT_HOST, INPUT_PORT))
+                output_socket.bind((OUTPUT_HOST, OUTPUT_PORT))
 
-        input_socket.listen(1)
-        output_socket.listen(1)
+                input_socket.listen(1)
+                output_socket.listen(1)
 
-        print("Waiting for input connection ...")
-        input_conn, input_addr = input_socket.accept()
-        print("Input connected.")
-        print("Waiting for output connection ...")
-        output_conn, output_addr = output_socket.accept()
-        print("Output connected.")
+                print("Waiting for input connection ...")
+                input_conn, input_addr = input_socket.accept()
+                print("Input connected.")
+                print("Waiting for output connection ...")
+                output_conn, output_addr = output_socket.accept()
+                print("Output connected.")
 
-        with input_conn:
-            print('Input connected by', input_addr)
-            with output_conn:
-            #     print('put connected by', output_addr)
+                with input_conn:
+                    print('Input connected by', input_addr)
+                    with output_conn:
+                    #     print('put connected by', output_addr)
 
-                while True:
-                    package = input_conn.recv(1024)
-                    # if not package:
-                    #     break
-                    print(package)
-                    output_conn.send(package)
+                        while True:
+                            package = input_conn.recv(1024)
+                            # if not package:
+                            #     break
+                            print(package)
+                            output_conn.send(package)
 
-        input_socket.close()
-        output_socket.close()
-
-        sleep(3)
     except ConnectionResetError as e:
         print(e)
 
